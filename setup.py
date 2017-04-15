e142927be4d7178d7da1df307681d69cc675cc0e
lore <lorenzomartino86@gmail.com>
2017-04-15 17:15:18 +0200

    removed dependency

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup
 
 setup (
-    # ...
+    name='ecommerce-purchases',
+    version=0.1,
+    description='Sample application of Pandas library',
     test_suite = 'nose.collector',
     install_requires=['pandas']
 )
