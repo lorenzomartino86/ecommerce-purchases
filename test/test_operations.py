e3452b4d296f5269a8140a421ef1bc9461a7d341
lore <lorenzomartino86@gmail.com>
2017-04-18 21:37:36 +0200

    added more operations tests

---
+++ test/test_operations.py
@@ -0,0 +1,36 @@
+import unittest
+import numpy as np
+import pandas as pd
+
+class OperationsTest(unittest.TestCase):
+
+    def setUp(self):
+        data = {'Company':['GOOG','GOOG','MSFT','MSFT','FB','FB'],
+                'Person':['Sam','Charlie','Amy','Vanessa','Carl','Sarah'],
+                'Sales':[200,120,340,124,243,350]}
+        self.data_frame = pd.DataFrame(data)
+
+    def test_distinct_company(self):
+        unique = self.data_frame['Company'].nunique()
+        self.assertEqual(3, unique)
+
+    def test_apply_times2(self):
+        data_frame = self.data_frame['Sales'].apply(self.__times2)
+        self.assertEqual(400, data_frame[0])
+        self.assertEqual(248, data_frame[3])
+        self.assertEqual(700, data_frame[5])
+
+    def test_apply_len(self):
+        data_frame = self.data_frame['Company'].apply(len)
+        self.assertEqual(4, data_frame[0])
+        self.assertEqual(4, data_frame[3])
+        self.assertEqual(2, data_frame[5])
+
+    def test_apply_lambda(self):
+        data_frame = self.data_frame['Sales'].apply(lambda x: x*3)
+        self.assertEqual(600, data_frame[0])
+        self.assertEqual(372, data_frame[3])
+        self.assertEqual(1050, data_frame[5])
+
+    def __times2(self, x):
+        return x*2
