f525f18865e78ec7bc286ddf877eada0f817f5cd
lore <lorenzomartino86@gmail.com>
2017-04-22 14:02:44 +0200

    added:    - read and load csv    - xlrd requirement

--- test/test_in_out.py
+++ test/test_in_out.py
@@ -6,6 +6,21 @@ class InputOutTest(unittest.TestCase):
 
     def setUp(self):
         self.file_location = os.path.join(os.path.dirname(__file__), 'EcommercePurchases.csv')
+        self.loaded_csv = os.path.join(os.path.dirname(__file__), 'test_load')
 
     def test_read_csv(self):
         data_frame = pd.read_csv(self.file_location)
+        dimensions = data_frame.shape
+        total_rows = len(data_frame.index)
+        self.assertEqual(10000, total_rows)
+        self.assertEqual((10000, 14), dimensions)
+
+    def test_load_csv(self):
+        data_frame = pd.read_csv(self.file_location)
+        data_frame.to_csv(self.loaded_csv, index=False)
+        data_frame_loaded = pd.read_csv(self.loaded_csv)
+        dimensions = data_frame_loaded.shape
+        total_rows = len(data_frame_loaded.index)
+        self.assertEqual(10000, total_rows)
+        self.assertEqual((10000, 14), dimensions)
+        os.remove(self.loaded_csv)
